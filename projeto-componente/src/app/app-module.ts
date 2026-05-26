4f019d022590b99becbc9968bc7ecd86faefe650
Victor R. Silva <victorr.silva005@gmail.com>
2026-05-26 16:18:59 -0300

    Criado componentes filhos

--- projeto-componente/src/app/app-module.ts
+++ projeto-componente/src/app/app-module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Card } from './card/card';
 import { CardRoxo } from './card-roxo/card-roxo';
+import { CardButton } from './card-button/card-button';
+import { CardRoxoButton } from './card-roxo-button/card-roxo-button';
 
 @NgModule({
-  declarations: [App, Card, CardRoxo],
+  declarations: [App, Card, CardRoxo, CardButton, CardRoxoButton],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
