f04ca700f611dfab1d28a0867ee5cfff89334f1c
Victor R. Silva <victorr.silva005@gmail.com>
2026-05-27 11:18:27 -0300

    Implementado template dentro do componente

--- projeto-componente/src/app/card-button/card-button.ts
+++ projeto-componente/src/app/card-button/card-button.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button',
   standalone: false,
-  templateUrl: './card-button.html',
+  // templateUrl: './card-button.html',
+  template: `
+    <div class="card-button">Adquirir</div>
+  `,
   styleUrl: './card-button.scss',
 })
 export class CardButton {}
