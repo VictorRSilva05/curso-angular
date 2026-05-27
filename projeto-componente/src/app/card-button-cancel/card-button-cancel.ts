aaaa7817c18da6395663a734daccbb9255730046
Victor R. Silva <victorr.silva005@gmail.com>
2026-05-27 11:32:39 -0300

    Implmentado estilização dentro do componente

---
+++ projeto-componente/src/app/card-button-cancel/card-button-cancel.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-button-cancel',
+  standalone: false,
+  templateUrl: './card-button-cancel.html',
+  //styleUrl: './card-button-cancel.scss',
+  styles: [
+    `
+        .card-cancel-button{
+        font-size: 16px;
+        text-align: center;
+        margin-top: 10px;
+        border-radius: 25px;
+        background-color: red;
+        cursor: pointer;
+        font-weight: bold;
+        padding: 10px;
+    }
+    `
+  ]
+})
+export class CardButtonCancel {}
--- projeto-componente/src/app/cards/cards-module.ts
+++ projeto-componente/src/app/cards/cards-module.ts
@@ -4,9 +4,10 @@ import { CardButton } from '../card-button/card-button';
 import { CardRoxoButton } from '../card-roxo-button/card-roxo-button';
 import { CardRoxo } from '../card-roxo/card-roxo';
 import { Card } from '../card/card';
+import { CardButtonCancel } from '../card-button-cancel/card-button-cancel';
 
 @NgModule({
-  declarations: [ Card, CardRoxo, CardButton, CardRoxoButton],
+  declarations: [ Card, CardRoxo, CardButton, CardRoxoButton, CardButtonCancel],
   imports: [CommonModule],
   exports:[Card, CardRoxo, CardButton, CardRoxoButton]
 })
