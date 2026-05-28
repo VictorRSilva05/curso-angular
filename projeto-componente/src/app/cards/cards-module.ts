ee4c87b773335e10d61089001376a4dabbb51d7d
Victor R. Silva <victorr.silva005@gmail.com>
2026-05-28 11:29:13 -0300

    Inspeção de elementos e utilizando ::ng-deep e :host em componentes externos

--- projeto-componente/src/app/cards/cards-module.ts
+++ projeto-componente/src/app/cards/cards-module.ts
@@ -5,10 +5,11 @@ import { CardRoxoButton } from '../card-roxo-button/card-roxo-button';
 import { CardRoxo } from '../card-roxo/card-roxo';
 import { Card } from '../card/card';
 import { CardButtonCancel } from '../card-button-cancel/card-button-cancel';
+import {MatSliderModule} from '@angular/material/slider';
 
 @NgModule({
   declarations: [ Card, CardRoxo, CardButton, CardRoxoButton, CardButtonCancel],
-  imports: [CommonModule],
+  imports: [CommonModule, MatSliderModule],
   exports:[Card, CardRoxo, CardButton, CardRoxoButton]
 })
 export class CardsModule {}
