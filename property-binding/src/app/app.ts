ce84a8df098581869a4442ebf999b61dc882c4dd
Victor R. Silva <victorr.silva005@gmail.com>
2026-06-02 13:33:43 -0300

    Utilizando classes de css para realizar o binding em elementos HTML

--- property-binding/src/app/app.ts
+++ property-binding/src/app/app.ts
@@ -9,29 +9,6 @@ import { Component, signal } from '@angular/core';
 export class App {
   protected readonly title = signal('property-binding');
 
-  widthButton1 = '110px';
-  widthButton2 = 130;
-  stylesObj = {
-    width: '160px',
-    backgroundColor: 'grey',
-  };
-
-  updateStyleObj(){
-    console.log('updateStyleObj');
-    this.stylesObj.width = '170px';
-    this.stylesObj.backgroundColor = 'lightblue';
-  }
-
-  updateStyleObjectCorrect(){
-    console.log('updateStyleObjectCorrect');
-
-    this.stylesObj = {
-      width: '170px',
-      backgroundColor: 'lightblue',
-    };
-  }
-
-  updateWidth(){
-    this.widthButton2 = 200;
-  }
+    isRedButton = true;
+    isGreen = true;
 }
